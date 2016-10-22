be95b76f162615227b7413e3b5c793b8f9867ce5
Ex Good <runic.meditation@gmail.com>
2016-10-23 01:15:30 +0300

    Finish the implementation.

--- goetia.js
+++ goetia.js
@@ -1,1 +1,9 @@
-console.log(1);
+var allDemons = document.querySelectorAll('.hideDemon');
+var firstImage = document.createElement('img');
+var container = document.querySelector('.container');
+firstImage.setAttribute('class', 'visibleDemon');
+firstImage.setAttribute('src', allDemons[(Math.floor(Math.random() * allDemons.length))].getAttribute('src'));
+container.appendChild(firstImage);
+setInterval(function _conjure() {
+    this.setAttribute('src', allDemons[(Math.floor(Math.random() * allDemons.length))].getAttribute('src'));
+}.bind(firstImage), 60000);
